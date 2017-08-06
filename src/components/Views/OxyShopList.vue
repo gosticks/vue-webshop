8c86a8a6ccbac8d726c8cab8e0a4be8e29b15ab6
Wlad <wlad.meixner@protonmail.com>
2017-08-07 01:24:59 +0200

    Reworked responsive layout for overview & added rating to items

--- src/components/Views/OxyShopList.vue
+++ src/components/Views/OxyShopList.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="container">
     <p>This shop is work in progress!</p>
-    <div class="row" v-if="products.length > 0">
-      <div v-for="item of products" class="four columns" @click="itemSelected(item.id)"> 
+    <div class="row product-list" v-if="products.length > 0">
+      <div v-for="item of products" class="columns" @click="itemSelected(item.id)"> 
         <div class="item product">
           <img class="product-thumb" :src="item.thumb">
-          <p class="product-name">{{item.name}}</p>
+          <p class="product-name">{{item.title}}</p>
+          <oxy-rating :v-if="item.rating" :rating="item.rating"></oxy-rating>
           <p class="product-price">{{item.price | currency }}</p>
         </div>
       </div>
@@ -15,8 +16,13 @@
 
 <script>
 import shop from '../../store/modules/shop-mockup'
+import OxyRating from '../UIComponents/OxyRating'
 
 export default {
+  components: {
+    OxyRating
+  },
+
   methods: {
     priceString (priceObj) {
       return priceObj.usd + ' $'
@@ -45,6 +51,27 @@ export default {
 </script>
 
 <style lang="scss">
+
+  .product-list {
+    .columns, .column{
+      margin: 0;
+      padding: 12px;
+      width:100%;
+
+      @media (min-width: 768px) {
+        width: 50%;
+      }
+
+      @media (min-width: 1024px) {
+        width: 33%;
+      }
+
+      @media (min-width: 1920px) {
+        width: 25%;
+      }
+    }
+  }
+
   .item.product {
     text-align: center;
     cursor: pointer;
--- src/store/modules/shop-mockup.js
+++ src/store/modules/shop-mockup.js
@@ -2,9 +2,10 @@
  * Mocking client-server processing
  */
 const _products = [
-  {'id': 1, 'title': 'iPad 4 Mini', 'price': 500.01, 'thumb': 'https://unsplash.it/300/300', 'inventory': 2},
-  {'id': 2, 'title': 'H&M T-Shirt White', 'price': 10.99, 'thumb': 'https://unsplash.it/400/400', 'inventory': 10},
-  {'id': 3, 'title': 'Charli XCX - Sucker CD', 'price': 19.99, 'thumb': 'https://unsplash.it/300/300', 'inventory': 5}
+  {'id': 1, 'title': 'iPad 4 Mini', 'price': 500.01, 'thumb': 'https://unsplash.it/300/300', 'inventory': 2, 'rating': 4.2},
+  {'id': 2, 'title': 'H&M T-Shirt White', 'price': 10.99, 'thumb': 'https://unsplash.it/400/400', 'inventory': 10, 'rating': 2},
+  {'id': 3, 'title': 'Charli XCX - Sucker CD', 'price': 19.99, 'thumb': 'https://unsplash.it/300/300', 'inventory': 5, 'rating': 3.5},
+  {'id': 4, 'title': 'Elphant T-Shirt Brown', 'price': 199.99, 'thumb': 'https://unsplash.it/600/600', 'inventory': 10, 'rating': 4.9}
 ]
 
 export default {
