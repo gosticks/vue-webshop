979c6e13c10a98e29d5fc9556def3f0a99e2247d
Wlad <wlad.meixner@protonmail.com>
2017-08-07 01:23:34 +0200

    Added OxyRating simple vue rating module using stars.

---
+++ src/components/UIComponents/OxyRating.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="rating-indicator">
+    <i v-for="(_, index) in totalStart" :class="iconForStar(index)">
+    </i>
+  </div>
+</template>
+
+<script>
+const numberStars = 5
+
+export default {
+  props: {
+    editable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    rating: {
+      type: Number,
+      required: true
+    }
+  },
+  data () {
+    return {
+      totalStart: numberStars
+    }
+  },
+
+  methods: {
+    iconForStar: function (index) {
+      let v = (this.rating - 1.0) - index
+      console.debug(index, v, this.rating)
+      if (v >= -0.3) {
+        return 'ion-ios-star'
+      } else if (v >= -0.5) {
+        return 'ion-ios-star-half'
+      }
+      return 'ion-ios-star-outline'
+    }
+  }
+}
+</script>
+
+<style>
+  .rating-indicator {
+    font-size: 2rem;
+    color: #888;
+    display: inline-block;
+    text-align: center;
+  }
+</style>
